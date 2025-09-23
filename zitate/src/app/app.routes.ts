8989ec2c04a59dc00f788dec0431caa59e44c963
Lisa Hausenblas <lisiknuff@MacBook-Air-Lisa-H.fritz.box>
2025-09-23 11:29:21 +0200

    app Componenten home,nav, zitat-hinzufuegen, zitat-sammlung bearbeitet

--- zitate/src/app/app.routes.ts
+++ zitate/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { Nav } from './nav/nav';
+import { Home } from './home/home';
 import { ZitatHinzufuegen } from './zitat-hinzufuegen/zitat-hinzufuegen';
 import { ZitateSammlung } from './zitate-sammlung/zitate-sammlung';
 
@@ -10,7 +11,7 @@ export const routes: Routes = [
     { path: "zitate-sammlung", component: ZitateSammlung },
     { path: "zitate-sammlung/:id", component: ZitateSammlung },
     //{ path: "login", component: LoginComponent },
-    //{ path: "", component: HomeComponent, pathMatch: 'full' },
+    { path: "", component: Home},
     { path: "**", redirectTo: "" }
 ];
 
